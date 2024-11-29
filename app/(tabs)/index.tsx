3f7b9bda6a6b0663017dd324f10ae0fcc359c62a
Delmar Filho <ddlmar13@gmail.com>
2024-11-29 12:55:27 -0300

    create repository

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,56 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useState } from "react";
 
 export default function TabOneScreen() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  const [scannedCode, setScannedCode] = useState("");
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <CameraView
+        style={[StyleSheet.absoluteFillObject, { flex: 1 }]}
+        barcodeScannerSettings={{
+          barcodeTypes: ["itf14"],
+        }}
+        onBarcodeScanned={
+          scannedCode ? undefined : ({ data }) => setScannedCode(data)
+        }
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            padding: 10,
+          }}
+        >
+          {scannedCode}
+        </Text>
+
+        <Button
+          disabled={!scannedCode}
+          title="Scan again"
+          onPress={() => setScannedCode("")}
+        />
+      </CameraView>
     </View>
   );
 }
@@ -16,16 +58,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
